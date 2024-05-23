69216fd20097336089d2ce82661559c13336c407
Saket Suman <ssaket121@gmail.com>
2024-05-23 13:34:47 -0400

    added routing and responsive frontend

---
+++ src/app/home-page-mobile/home-page-mobile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-page-mobile',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-page-mobile.component.html',
+  styleUrl: './home-page-mobile.component.css'
+})
+export class HomePageMobileComponent {
+  imagePath = "assets/images/saket.png";
+}
---
+++ src/app/nav-bar-mobile/nav-bar-mobile.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NavBarMobileComponent } from './nav-bar-mobile.component';
+
+describe('NavBarMobileComponent', () => {
+  let component: NavBarMobileComponent;
+  let fixture: ComponentFixture<NavBarMobileComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [NavBarMobileComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(NavBarMobileComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
